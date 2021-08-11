a731c1b632501c2ba94d58b91d1c093bb4a8cbb1
Migon <migon99@naver.com>
2021-08-11 11:19:38 +0900

    linear regression

--- preprocessing/linear_regression.py
+++ preprocessing/linear_regression.py
@@ -2,21 +2,28 @@ import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
-digits = pd.read_csv('C:/Users/user/Downloads/Data_linear.csv')
+data_linear = pd.read_csv('C:/Users/user/Downloads/Data_linear.csv')
+data_polynomial = pd.read_csv('C:/Users/user/Downloads/Data_polynomial.csv')
 
 clf = linear_model.LinearRegression()
 
-x, y = digits.iloc[:-30,2:-1], digits.iloc[:-30,-1]
-
-clf.fit(x, y)
-
-y_pred = clf.predict(digits.iloc[-30:,2:-1])
-y_true = digits.iloc[-30:,-1]
+x1, y1 = data_linear.iloc[:-20,2:-1], data_linear.iloc[:-20,-1]
+x2, y2 = data_polynomial.iloc[:-20,2:-1], data_polynomial.iloc[:-20,-1]
 
 fig = plt.figure(figsize=(8,8)) ## 캔버스 생성
 fig.set_facecolor('white') ## 캔버스 색상 설정
 ax = fig.add_subplot() ## 그림 뼈대(프레임) 생성
 
-ax.plot(digits.iloc[-30:,1], y_pred)
-ax.plot(digits.iloc[-30:,1], y_true)
+clf.fit(x1, y1)
+y_pred1 = clf.predict(data_linear.iloc[-20:,2:-1])
+ax.plot(data_linear.iloc[-20:,1], y_pred1, label='Linear regression_linear')
+
+clf.fit(x2, y2)
+y_pred2 = clf.predict(data_polynomial.iloc[-20:,2:-1])
+ax.plot(data_linear.iloc[-20:,1], y_pred2, label='Linear regression_polynomial')
+
+y_true = data_linear.iloc[-20:,-1]
+ax.plot(data_linear.iloc[-20:,1], y_true)
+
+ax.legend()
 plt.show()
